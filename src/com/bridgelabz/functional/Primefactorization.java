0414d9908438308c9022e180d6dfd1d093d2d76c
Salina73 <kirankumar242018@gmail.com>
2019-08-05 17:50:36 +0530

    Programs

--- src/com/bridgelabz/functional/Primefactorization.java
+++ src/com/bridgelabz/functional/Primefactorization.java
@@ -7,15 +7,14 @@ import com.bridgelabz.utility.Utility;
 public class Primefactorization {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner scanner=new Scanner(System.in);
-Utility utility=new Utility();
+	
+		Scanner scanner=new Scanner(System.in);
+		Utility utility=new Utility();
 
-int number;
-System.out.println("enter the number");
-number=scanner.nextInt();
-
-utility.fact(number);
+		int number;
+		System.out.println("enter the number");
+		number=scanner.nextInt();
+		utility.fact(number);
 	}
 
 }
